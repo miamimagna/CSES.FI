70f27dec27f94e1375e408c3389482457223f3d4
miamimagna <138129055+miamimagna@users.noreply.github.com>
2024-10-15 01:40:46 +0530

    Update 079. Round Trip.cpp

--- 079. Round Trip.cpp
+++ 079. Round Trip.cpp
@@ -12,6 +12,61 @@ using v = vector<T>;
 #define sz size()
 #define int long long
 int mod = 1e9 + 7;
+/*
+another solution to this problem is using graph coloring algorithm, to ensure you have cycle with two or more node, we can check following condition:
+parent[cycleend] should not be cyclestart
+int cyclestart = -1, cycleend = -1;
+bool dfs(int node, v<v<int>>& adj, v<int>& color, v<int>& parent){
+    color[node] = 1;
+    for(int next: adj[node]){
+        if(!color[next]){
+            parent[next] = node;
+            if(dfs(next, adj, color, parent))
+                return true;
+        }
+        if(color[next] == 1 && parent[node] != next){
+            parent[next] = -1;
+            cyclestart = node;
+            cycleend = next;
+            return true;
+        }
+    }
+    color[node] = 2;
+    return false;
+}
+void solve(){
+    int n, m;
+    cin >> n >> m;
+    v<v<int>> adj(n + 1);
+    while(m--){
+        int a, b;
+        cin >> a >> b;
+        adj[a].pb(b);
+        adj[b].pb(a);
+    }
+    v<int> color(n + 1), parent(n + 1, -1);
+    bool found = 0;
+    for(int i = 1; i <= n; i++)
+        if(!color[i] && dfs(i, adj, color, parent)){
+            found = 1;
+            break;
+        }
+    if(found){
+        int cur = cyclestart;
+        v<int> ans{cur};
+        while(cur != cycleend){
+            cur = parent[cur];
+            ans.push_back(cur);
+        }
+        ans.push_back(ans.front());
+        reverse(ans.begin(), ans.end());
+        cout << ans.sz << endl;
+        for(auto x: ans) cout << x << ' '; 
+        cout << endl;
+    }
+    else cout << "IMPOSSIBLE" << endl;
+}
+*/
 // here possible denotes existence of solution and conti will tell us when to stop adding nodes to answer
 bool possible = false, conti = true;
 v<int> ans;
