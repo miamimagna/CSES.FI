70dd1b661354bb976c6bf319d2cc7f8929780056
miamimagna <138129055+miamimagna@users.noreply.github.com>
2025-01-13 16:39:28 +0530

    Update 142. Fixed-Length Paths I.cpp

--- 142. Fixed-Length Paths I.cpp
+++ 142. Fixed-Length Paths I.cpp
@@ -71,3 +71,70 @@ signed main()
     decompose_tree(1);
     cout << ans << endl;
 }
+/*
+this one is easier to understand
+#include <iostream>
+#include <vector>
+#include <deque>
+using namespace std;
+#define int long long
+template <typename t>
+using v = vector<t>;
+#define N 200001
+v<int> adj[N];
+int n, k;
+long long ans = 0;
+
+int suf(deque<int> &suf, int i)
+{
+    if (i < 0)
+        return suf[0];
+    if (i >= (int)suf.size())
+        return 0;
+    return suf[i];
+}
+
+void mergeSuffixes(deque<int> &sa, deque<int> &sb)
+{
+    if (sa.size() < sb.size())
+        swap(sa, sb);
+
+    for (size_t i = 0; i < sb.size(); i++)
+        ans += 1ll * (suf(sb, i) - suf(sb, i + 1)) * (suf(sa, k - i) - suf(sa, k - i + 1));
+
+    for (size_t i = 0; i < sb.size(); i++)
+        sa[i] += sb[i];
+}
+
+deque<int> dfs(int node, int p = -1)
+{
+    // depth 0 has one element
+    deque<int> suf_parents{1};
+    for (int child : adj[node])
+        if (child != p)
+        {
+            auto cq = dfs(child, node);
+            // pushing the entire child tree by 1
+            cq.push_front(cq.front());
+            mergeSuffixes(suf_parents, cq);
+        }
+    return suf_parents;
+}
+signed main()
+{
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+#endif
+    cin.tie(0)->sync_with_stdio(0);
+    cin >> n >> k;
+    for (int i = 1; i < n; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+    dfs(1);
+    cout << ans << endl;
+}
+*/
